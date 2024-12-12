65a275fef100a5914548dc1d5514a7ad26a2b1ad
MrZaxter <SymphonyOfFinality@gmail.com>
2024-12-13 01:12:04 +0300

    fix profile reports

--- src/Components/Main/Profile/ProfileRelationShip/ProfileRelationShip.tsx
+++ src/Components/Main/Profile/ProfileRelationShip/ProfileRelationShip.tsx
@@ -7,7 +7,6 @@ import {useSelector} from "react-redux";
 import {useParams} from "react-router-dom";
 import {RootState} from "../../../../redux";
 import parse from "html-react-parser";
-import TextOverflow from "../../../TextOverflow/TextOverflow.tsx";
 
 interface IAchieve {
     cover: string,
@@ -398,7 +397,12 @@ const ProfileRelationShip: React.FC = () => {
                                                                 <div>
                                                                     <span>Отчёт от {reformDate(report.report_date as string)}</span>
                                                                     <div className={'report_span_root'}>
-                                                                        <TextOverflow maxHeight={9999} text={report?.text} />
+                                                                        {parse((report.text
+                                                                            .split('\n')
+                                                                            .map((line, index) =>
+                                                                                `${index > 0 ? '\n' : ''}${line[0] !== '<' ? '&emsp;&emsp;' : ''}${line}`
+                                                                            )).join(''))
+                                                                        }
                                                                     </div>
                                                                 </div>
                                                                 {report?.image &&
